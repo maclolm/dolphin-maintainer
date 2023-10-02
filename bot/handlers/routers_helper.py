3427a70fffb13c265a6318e977d0c1436986bbfd
malcolm <berrrzin@yandex.ru>
2023-10-02 21:02:27 +0300

    актуализация имён пользователей за 2 запроса к telegram-серверу вне зависимости от количества пользователей в базе

--- bot/handlers/routers_helper.py
+++ bot/handlers/routers_helper.py
@@ -9,16 +9,16 @@ from logging import getLogger
 log = getLogger('owner_handler')
 
 
-async def get_expired_usernames(users: dict, session_data: SessionData):
+async def get_expired_usernames(users: list, session_data: SessionData):
     async with TelegramClient(
             session_data.session_username,
             session_data.api_id,
             session_data.api_hash) as client:
-        for user_tg_id in users.keys():
-            user = await client.get_entity(user_tg_id)
-            username = "@" + user.username
-            if users[user_tg_id] != username:
-                yield user_tg_id, username
+        for tg_id, username in users:
+            user = await client.get_entity(tg_id)
+            actual_username = "@" + user.username
+            if username != actual_username:
+                yield tg_id, actual_username
 
 
 async def get_username(tg_id, session_data: SessionData):
@@ -46,22 +46,12 @@ async def get_user_id(username, session_data: SessionData):
 
 
 async def refresh_all_users(db_conn: DataBaseController, session_data: SessionData):
-    subs = db_conn.get_all_subs()
-    # for tg_id
+    subs = db_conn.get_all_subs_usernames()
+    subs_with_expired_username = get_expired_usernames(subs, session_data)
+    async for tg_id, username in subs_with_expired_username:
+        db_conn.update_sub_tg_username(tg_id, username)
 
-    for _, tg_id, username, _, _, _ in subs:
-        actual_username = await get_username(tg_id, session_data)
-        actual_username = "@" + actual_username
-        if username != actual_username:
-            db_conn.update_sub_tg_username(tg_id, actual_username)
-            log.info(f'Telegram username of sub {username} has been updated [{username}] --> {actual_username}')
-
-    owners = db_conn.get_all_owners()
-    for _, tg_id, username in owners:
-        actual_username = await get_username(tg_id, session_data)
-        actual_username = "@" + actual_username
-        if username != actual_username:
-            db_conn.update_owner_tg_username(tg_id, actual_username)
-            log.info(f'Telegram ID of username {username} has been updated [{username}] --> {actual_username}')
-
-
+    owners = db_conn.get_all_owners_usernames()
+    owners_with_expired_username = get_expired_usernames(owners, session_data)
+    async for tg_id, username in owners_with_expired_username:
+        db_conn.update_sub_tg_username(tg_id, username)
--- dbcontroller/sql_actions.py
+++ dbcontroller/sql_actions.py
@@ -2,23 +2,25 @@ from datatypes import SubStatus
 
 
 class SQlActions:
-    GET_ALL_SUBS = "SELECT * FROM subscribers"
-    ADD_TO_SUBS = "INSERT INTO subscribers (tg_id, username, creation_datetime, status, sub_days) " \
-                  "VALUES ({user_id:d}, '{username:s}', '{datetime}', {status}, {sub_days:d});"
-    DELETE_FROM_SUBS = "DELETE FROM subscribers WHERE tg_id={user_id:d}"
-    GET_SUBS_IDS = "SELECT tg_id FROM subscribers;"
-    GET_SUB_DAYS = "SELECT sub_days FROM subscribers WHERE tg_id = {tg_id:d};"
-    GET_SUB_STATS = "SELECT sub_days, status FROM subscribers WHERE tg_id = '{tg_id:d}';"
+    GET_ALL_SUBS           = "SELECT * FROM subscribers"
+    ADD_TO_SUBS            = "INSERT INTO subscribers (tg_id, username, creation_datetime, status, sub_days) " \
+                             "VALUES ({user_id:d}, '{username:s}', '{datetime}', {status}, {sub_days:d});"
+    DELETE_FROM_SUBS       = "DELETE FROM subscribers WHERE tg_id={user_id:d}"
+    GET_SUBS_IDS           = "SELECT tg_id FROM subscribers;"
+    GET_SUBS_USERNAMES     = "SELECT tg_id, username FROM subscribers;"
+    GET_SUB_DAYS           = "SELECT sub_days FROM subscribers WHERE tg_id = {tg_id:d};"
+    GET_SUB_STATS          = "SELECT sub_days, status FROM subscribers WHERE tg_id = '{tg_id:d}';"
     UPDATE_SUB_TG_USERNAME = "UPDATE subscribers SET username = '{actual_username:s}' WHERE tg_id = {tg_id:d}"
-    DECREASE_SUB_DAYS = "UPDATE subscribers SET sub_days = sub_days - 1 WHERE sub_days > 0"
-    RECALC_SUB_STATUS = "UPDATE subscribers SET status = CASE " \
-                        f"WHEN (sub_days = 0) THEN {SubStatus.EXPIRED} " \
-                        f"WHEN (sub_days < 3 AND sub_days > 0) THEN {SubStatus.EXPIRED_SOON} END;"
-    GET_EXPIRED_SUBS = "SELECT tg_id, sub_days FROM subscribers " \
-                       f"WHERE status = {SubStatus.EXPIRED} OR status = {SubStatus.EXPIRED_SOON}"
+    DECREASE_SUB_DAYS      = "UPDATE subscribers SET sub_days = sub_days - 1 WHERE sub_days > 0"
+    RECALC_SUB_STATUS      = "UPDATE subscribers SET status = CASE " \
+                             f"WHEN (sub_days = 0) THEN {SubStatus.EXPIRED} " \
+                             f"WHEN (sub_days < 3 AND sub_days > 0) THEN {SubStatus.EXPIRED_SOON} END;"
+    GET_EXPIRED_SUBS       = "SELECT tg_id, sub_days FROM subscribers " \
+                             f"WHERE status = {SubStatus.EXPIRED} OR status = {SubStatus.EXPIRED_SOON}"
 
-    ADD_TO_OWNERS = "INSERT INTO owners (tg_id, username) VALUES ({user_id:d}, '{username:s}');"
-    DELETE_FROM_OWNERS = "DELETE FROM owners WHERE tg_id={user_id:d}"
-    GET_ALL_OWNERS = "SELECT * FROM owners"
-    GET_OWNERS_IDS = "SELECT tg_id FROM owners;"
+    ADD_TO_OWNERS            = "INSERT INTO owners (tg_id, username) VALUES ({user_id:d}, '{username:s}');"
+    DELETE_FROM_OWNERS       = "DELETE FROM owners WHERE tg_id={user_id:d}"
+    GET_ALL_OWNERS           = "SELECT * FROM owners"
+    GET_OWNERS_IDS           = "SELECT tg_id FROM owners;"
+    GET_OWNERS_USERNAMES     = "SELECT tg_id, username FROM owners;"
     UPDATE_OWNER_TG_USERNAME = "UPDATE owners SET username = '{actual_username:s}' WHERE tg_id = {tg_id:d}"
